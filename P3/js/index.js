3c3cb83be3a2c335a702f522b17f17d24b747282
hilmarc <31723052+hilmarc@users.noreply.github.com>
2017-10-22 20:54:13 +0200

    Nav.bar og logo
    JS-kode til navbar lagt i eget dokument.

--- P3/js/index.js
+++ P3/js/index.js
@@ -1,5 +1,5 @@
 //slideshow script
-/*
+
 var myIndex = 0;
 carousel();
 
@@ -14,30 +14,5 @@ function carousel() {
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 9000);    
 }
-*/
 
-//Nav.bar script
-
-function toggle_meny(nav_name, nav_button_1, nav_button_2){
-    var nav_bar = document.getElementById(nav_name);
-    var nav_elem_1 = document.getElementById(nav_button_1);
-    var nav_elem_2 = document.getElementById(nav_button_2);
-    
-    if(nav_bar.style.display === "block"){
-        nav_bar.style.display = "none";
-        nav_elem_1.style.backgroundColor = "#1c3f26";
-    }
-    else{
-        nav_bar.style.display = "block";
-        nav_elem_1.style.backgroundColor = "#b2722a";
-        nav_elem_2.style.backgroundColor = "#1c3f26";
-    }
-}
-
-function close_meny(nav_name, nav_button){
-    var nav_bar = document.getElementById(nav_name);
-    var nav_elem = document.getElementById(nav_button);
-    nav_bar.style.display = "none";
-    nav_elem.style.backgroundColor = "#1c3f26";
-}
 
