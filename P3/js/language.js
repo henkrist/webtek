626ec5cd8f21c5a518683a974c8ffb7b739e04ae
hilmarc <31723052+hilmarc@users.noreply.github.com>
2017-11-01 15:59:10 +0100

    created language.js

--- P3/js/language.js
+++ P3/js/language.js
@@ -1,1 +1,9 @@
 
+function switchLang(){
+    window.name ="no";
+    var class_name = document.getElementsByClassName(window.name);
+    for(var i = 0; i<class_name.length; i++){
+        class_name[i].style.display = "none";
+    }
+    
+}
