ae34f7db21cee4358de90b668c461b69a6dd8231
idabjo <31986943+idabjo@users.noreply.github.com>
2017-11-01 09:56:08 +0100

    Slideshow - JS

--- P3/js/slideshow.js
+++ P3/js/slideshow.js
@@ -1,11 +1,27 @@
 //slideshow script
 
-var running = 0 /* Used to check if fade is running */
+var images = document.getElementById('slideShow').children;
+var slideIndex = 1;
+var lastSlideIndex = images.length - 1;
 
-function StartFade() {
-  if (running != 1) {
-   running = 1
-   fadeOut()
-  }
+function rotateSlide() {
+    setTimeout(function() {
+        if (slideIndex == 0) {
+            var previousSlideIndex = lastSlideIndex;
+        } else {
+            var previousSlideIndex = slideIndex - 1;
+        }
+
+        images[previousSlideIndex].style = 'display: none';
+        images[slideIndex].style = 'display: inline';
+
+        if (slideIndex == lastSlideIndex) {
+            slideIndex = 0;
+        } else {
+            slideIndex++;
+        }
+        rotateSlide();
+    }, 5000);
 }
 
+rotateSlide();
