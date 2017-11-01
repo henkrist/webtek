d76cb4a0cdefeaa2bb37b706085b30c145981238
idabjo <31986943+idabjo@users.noreply.github.com>
2017-11-01 09:46:36 +0100

    Slideshow - CSS

--- P3/js/slideshow.js
+++ P3/js/slideshow.js
@@ -1,18 +1,11 @@
 //slideshow script
 
-var myIndex = 0;
-carousel();
+var running = 0 /* Used to check if fade is running */
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000);    
+function StartFade() {
+  if (running != 1) {
+   running = 1
+   fadeOut()
+  }
 }
-
 
