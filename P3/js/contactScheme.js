604d4b94d78583f8c3366aae1b2c9b2c26e71ad0
Hinken <henkrist96@gmail.com>
2017-11-05 13:27:39 +0100

    Contact page
    gjort maaaaaaassssee stuff

--- P3/js/contactScheme.js
+++ P3/js/contactScheme.js
@@ -1,8 +1,29 @@
 
 function validateForm(){
-  var formInsert ==  document.forms["myFrom"]["fname"].value
-  if (formInsert == "" || formInsert == " ") {
-    alert("Name has to be filled out");
-    return false;
-  }
+    var formInsert == document.forms["contact_from"].value;
+    
+    if (formInsert["fname"].value == "" || formInsert["fname"].value == " ") {
+        alert("Name has to be filled out");
+        return false;
+    }
+    else if(formInsert["email"].value == "" || formInsert["email"].value == " " || findat(formInsert["email"].value) == "false"{
+        alert("Insert a valid email adress");
+        return false;
+    }
+    else if (formInsert["message"].value == "" || formInsert["message"].value == " ") {
+        alert("You have to write something");
+        return false;
+    }
+}
+
+function findat(email){
+    for (var i=0;i<email.length;i++){
+      if(email[i] == "@"){
+          return true;
+          break;
+      }  
+        else{
+            return false;
+        }
+    }
 }
