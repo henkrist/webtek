ea85cd7b58b155e8593355bc7c243a7b3b9e5321
hilmarc <31723052+hilmarc@users.noreply.github.com>
2017-11-06 13:18:06 +0100

    språk

--- P3/js/language.js
+++ P3/js/language.js
@@ -1,9 +1,30 @@
+function initLang(){
+    if(sessionStorage.getItem("language") == null){
+        sessionStorage.setItem("language", "en");
+    }
+    setLang();
+}
 
-function switchLang(){
-    window.name ="no";
-    var class_name = document.getElementsByClassName(window.name);
-    for(var i = 0; i<class_name.length; i++){
-        class_name[i].style.display = "none";
+function switchLang(lang){
+    sessionStorage.setItem("language", lang);
+    setLang();
+}
+
+function setLang(){
+    var current_lang = document.getElementsByClassName(sessionStorage.getItem("language"));
+    var new_lang;
+    
+    if(sessionStorage.getItem("language") == 'en'){
+        new_lang = document.getElementsByClassName('no');
+    }else{
+        new_lang = document.getElementsByClassName('en');
     }
     
+    for(var i = 0; i<current_lang.length; i++){
+        current_lang[i].style.display = "block";
+    }
+    for(var i = 0; i<new_lang.length; i++){
+        new_lang[i].style.display = "none";
+    }
 }
+
