c9aa55b196af61f2ce825d2b8d08d7d5023417e0
hilmarc <31723052+hilmarc@users.noreply.github.com>
2017-11-07 14:39:57 +0100

    språk forklaring

--- P3/js/language.js
+++ P3/js/language.js
@@ -1,3 +1,5 @@
+//Initialiserende funsjon som blir kalt opp når body er lastet.
+//Sjekker om det er første gang siden har blitt besøkt 
 function initLang(){
     if(sessionStorage.getItem("language") == null){
         sessionStorage.setItem("language", "no");
@@ -5,11 +7,15 @@ function initLang(){
     setLang();
 }
 
+//Funksjon som skifter språk på siden til ønsket språk. 
 function switchLang(lang){
     sessionStorage.setItem("language", lang);
     setLang();
 }
 
+//Funksjon som setter alle elementene på siden til rett språk.
+//Dette gjøres ved å hide de elementene som er feil språk og
+// block de elementene som er rett språk.
 function setLang(){
     var current_lang = document.getElementsByClassName(sessionStorage.getItem("language"));
     var new_lang;
