96d020ab535fc97eab81d7ab1fa7031a7118183b
idabjo <31986943+idabjo@users.noreply.github.com>
2017-11-08 23:29:08 +0100

    fiksa bugs

--- P3/js/slideshow.js
+++ P3/js/slideshow.js
@@ -2,8 +2,14 @@
 
 //array of image paths
 var imagePaths = [
-    "img/bilde2.jpg", "img/bilde3.jpg", "img/bilde1.jpg"
+    "img/bilde2.jpg",
+    "img/bilde3.jpg", 
+    "img/bilde1.jpg"
 ];
+
+var imageWidth = document.body.clientWidth;
+var imageHeight = imageWidth / (3254 / 1428);
+
 
 //variables referring to canvas and canvas context
 var showCanvas = null;
@@ -17,28 +23,28 @@ var revealTimer;
 //saves current context of canvas, draws current image onto canvas, increments globalAlpha by 10%
 function revealImage() {
     showCanvasCtx.save();
-    showCanvasCtx.drawImage(img, 0, 0, 600, 400);
+    showCanvasCtx.drawImage(img, 0, 0, imageWidth, imageHeight);
     showCanvasCtx.globalAlpha += 0.1;
-    if (showCanvasCtx.globalAlpha >= 1.0)
+    if (showCanvasCtx.globalAlpha >= 0.99) {
         clearInterval(revealTimer);
-    showCanvasCtx.restore();
+        showCanvasCtx.restore();
+        setTimeout(switchImage, 3000);
+    }
 }
 
 //looks at current image index, gets its paths from array, points at src for current image index
 function switchImage() {
-    img.setAttribute('src',imagePaths[currentImage++]);
+    img.setAttribute('src', imagePaths[currentImage++]);
     if (currentImage >= imagePaths.length)
         currentImage = 0;
     showCanvasCtx.globalAlpha = 0.1;
     revealTimer = setInterval(revealImage, 100);
 }
 
+//action when window loads
 showCanvas = document.getElementById('showCanvas');
 showCanvasCtx = showCanvas.getContext('2d');
+showCanvas.width = imageWidth;
+showCanvas.height = imageHeight;
 
-img.setAttribute('width', '600');
-img.setAttribute('height', '400');
 switchImage();
-
-// start the animation
-setInterval(switchImage, 3000);
