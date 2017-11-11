6220dc1ab972ff6dc58b47ab6f9a00e9d0f0b754
idabjo <31986943+idabjo@users.noreply.github.com>
2017-11-11 22:42:23 +0100

    slideshow

--- P3/js/Slideshow2Index.js
+++ P3/js/Slideshow2Index.js
@@ -1,13 +1,9 @@
-var myIndex = 0;
-carousel();
+var slidesx = document.querySelectorAll('#slidesx .slidex');
+var currentSlidex = 0;
+var slideInterval = setInterval(nextSlidex,2000);
 
-function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-       x[i].style.display = "none";  
-    }
-    myIndex++;
-    if (myIndex > x.length) {myIndex = 1}    
-    x[myIndex-1].style.display = "block";  
-    setTimeout(carousel, 9000); 
+function nextSlidex(){
+    slidesx[currentSlidex].className = 'slidex';
+    currentSlidex = (currentSlidex+1)%slidesx.length;
+    slidesx[currentSlidex].className = 'slidex showingx';
+}
