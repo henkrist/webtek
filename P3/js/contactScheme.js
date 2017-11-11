ac49a77585c6ea25a438155d45ea3549c5823bb8
Hinken <henkrist96@gmail.com>
2017-11-11 17:43:23 +0100

    div stuff
    lagt til kommentarer

--- P3/js/contactScheme.js
+++ P3/js/contactScheme.js
@@ -1,21 +1,30 @@
+/*Funksjon tilhørende kontaktskjemaet*/
 
+
+/*Kjøres når "Submit"-knapen trykkes*/
 function validateForm(){
+    /*Henter data fra skjemaet*/
     var formInsert == document.forms["contact_from"].value;
     
+    /*Sjekker om navnet er noe rimlig*/
     if (formInsert["name"].value == "" || formInsert["name"].value == " ") {
         alert("Name has to be filled out");
         return false;
     }
+    /*Sjekker om emailen er noe rimlig. Funksjonen "findat" kalles, se under*/
     else if(formInsert["email"].value == "" || formInsert["email"].value == " " || findat(formInsert["email"].value) == "false"{
         alert("Insert a valid email adress");
         return false;
     }
+    /*sjekker at meldingene er noe rimlig*/
     else if (formInsert["message"].value == "" || formInsert["message"].value == " ") {
         alert("You have to write something");
         return false;
     }
 }
 
+
+/*Går gjennom det som er skrevet, og sjekker om det er en faktisk email med et @-symbol*/
 function findat(email){
     for (var i=0;i<email.length;i++){
       if(email[i] == "@"){
--- P3/js/map.js
+++ P3/js/map.js
@@ -1,4 +1,9 @@
 
+/*Kartfunksjonen er hentet fra Google på: https://developers.google.com/maps/documentation/javascript/adding-a-google-map 
+
+Vi har tilpasset kooridnatene og zommen, samt styling av boksen til vårt formål.
+For å kunne bruke kartet måtte vi gå til anskaffelse av en API-nøkkel.*/
+
 function initMap() {
   var uluru = {lat: 78.220547, lng: 15.641144};
   var map = new google.maps.Map(document.getElementById('map'), {
